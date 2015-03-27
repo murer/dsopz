b57a841fb0e7cbf57aad056d9d5e5000dd41e0ca
murer <murer@dextra-sw.com>
2015-03-27 10:26:41 -0300

    x

--- src/processor_indexed.py
+++ src/processor_indexed.py
@@ -10,6 +10,12 @@ class IndexedProcessor(processor.Processor):
 		self.columns = columns
 		self.indexed = indexed
 
+	def __setIndexed(self, column, indexed):
+		if column.get('indexed') == indexed:
+			return False
+		column['indexed'] = indexed
+		return True
+
 	def resolve(self):
 		print >> sys.stderr, 'process', self.processed
 		for ent in self.block:
@@ -18,11 +24,8 @@ class IndexedProcessor(processor.Processor):
 				column = ent['properties'].get(name)
 				if not column:
 					continue
-				print 'x', column.get('indexed'), self.indexed 
-				if column.get('indexed') == self.indexed:
-					continue
-				#column['indexed'] = self.indexed
-				changed = True
+				if self.__setIndexed(column, self.indexed):
+					changed = True
 			if changed:
 				print json.dumps(ent)
 
@@ -33,7 +36,7 @@ def __main():
 	parser.add_argument('-c', '--columns', required=True, nargs='+', help='columns')
 	parser.add_argument('-i', '--indexed', required=True, choices=('false', 'true'), help='indexed')
 	args = parser.parse_args()
-	processor = IndexedProcessor(args.columns, args.indexed, args.kinds)
+	processor = IndexedProcessor(args.columns, args.indexed == 'true', args.kinds)
 	processor.process()
 
 if __name__ == '__main__':
