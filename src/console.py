6998afb906f642122f59be8a3d8299e2e587140d
murer <murer@dextra-sw.com>
2015-04-02 14:58:23 -0300

    x

--- src/console.py
+++ src/console.py
@@ -3,16 +3,18 @@ import sys
 import reader
 import dsutil
 import json
+import cmd
 
-class Console(object):
+class Console(cmd.Cmd):
 
 	def __init__(self, dataset, namespace=None, prompt=True,source=sys.stdin, 
 			dest=sys.stdout, show_size=True, seperator='\t'):
+		cmd.Cmd.__init__(self, stdin=source, stdout=dest)
 		self.dataset = dataset
 		self.namespace = namespace
-		self.prompt = prompt
-		self.source = source
-		self.dest = dest
+		self.prompt = ''
+		if prompt:
+			self.prompt = '> '
 		self.show_size = show_size
 		self.seperator = seperator
 
@@ -25,31 +27,33 @@ class Console(object):
 				i = ent['properties'][p].get('indexed')
 				t = t.replace('Value', '')
 				line += '\t%s/%s/%s/%s' % (p, t, i, json.dumps(v))
-			print >> self.dest, line
+			print >> self.stdout, line
 
 	def process(self, gql):
 		result = reader.query(self.dataset, gql, namespace=self.namespace, limit=0)
 		self.show_entities(gql, result)
 		if self.show_size:
-			print >> self.dest, 'Total:', len(result['entities']), result['endCursor']
+			print >> self.stdout, 'Total:', len(result['entities']), result['endCursor']
 
-	def handle(self):
-		while True:
-			try:
-				if self.prompt:
-					self.dest.write('> ')
-				line = self.source.readline()
-				if not line:
-					break
-				line = line.strip()
-				if not line or line.startswith('#') or line.startswith('--'):
-					continue
-				if line == 'exit':
-					break
-				self.process(line)
-			except:
-				e = sys.exc_info()[0]
-				print >> self.dest, e
+	def do_select(self, line):
+		try:
+			gql = 'select ' + line
+			self.process(gql)
+		except:
+			e = sys.exc_info()[0]
+			print >> self.stdout, e
+
+	def do_EOF(self, line):
+		return self.do_exit('exit')
+
+	def do_exit(self, line):
+		return True
+
+	def default(self, line):
+		if line:
+			line = line.strip()
+		if not line.startswith('#') and not line.startswith('--'):
+			cmd.Cmd.default(self, line)
 
 def __main():
 	parser = argparse.ArgumentParser(description='Reader')
@@ -58,7 +62,11 @@ def __main():
 	parser.add_argument('-p', '--no-prompt', dest='prompt', action='store_false', help='no prompt')
 	args = parser.parse_args()
 	c = Console(args.dataset, namespace=args.namespace, prompt=args.prompt)
-	c.handle()
+	try:
+		c.cmdloop()
+	except KeyboardInterrupt:
+		return
+
 
 if __name__ == '__main__':
 	__main()
