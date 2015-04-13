5b6b9fe5c28fde6ce54274818c02b8d0859c4428
murer <murer@dextra-sw.com>
2015-04-13 10:07:01 -0300

    x

--- test/test.sh
+++ test/test.sh
@@ -7,15 +7,15 @@ EF=target/dsopz_test
 
 rm -rf "$EF" || true
 mkdir -p "$EF"    
-python src/dsopz.py 'export' -d "$DS" -n "$NS" -o true | python src/dsopz.py 'import' -d "$DS" -n "$NS" -o remove
+python dsopz/dsopz.py 'export' -d "$DS" -n "$NS" -o true | python dsopz/dsopz.py 'import' -d "$DS" -n "$NS" -o remove
 
-python src/dsopz.py 'import' -d "$DS" -n "$NS" -o upsert < "test/entities.json"
-python src/dsopz.py 'export' -d "$DS" -n "$NS" > "$EF/bak.json"
+python dsopz/dsopz.py 'import' -d "$DS" -n "$NS" -o upsert < "test/entities.json"
+python dsopz/dsopz.py 'export' -d "$DS" -n "$NS" > "$EF/bak.json"
 diff test/entities.json "$EF/bak.json"
 
-python src/dsopz.py 'index' -k "dsopz_test" -c c2 -i true > "$EF/processed.json" < "$EF/bak.json"
-python src/dsopz.py 'import' -d "$DS" -n "$NS" -o upsert < "$EF/processed.json"
-python src/dsopz.py 'gql' -d "$DS" -n "$NS" -q "select * from dsopz_test order by c2" > "$EF/other.json"
+python dsopz/dsopz.py 'index' -k "dsopz_test" -c c2 -i true > "$EF/processed.json" < "$EF/bak.json"
+python dsopz/dsopz.py 'import' -d "$DS" -n "$NS" -o upsert < "$EF/processed.json"
+python dsopz/dsopz.py 'gql' -d "$DS" -n "$NS" -q "select * from dsopz_test order by c2" > "$EF/other.json"
 if [ "x$(wc -l test/entities.json | cut -d" " -f1)" != "x2" ]; then
     echo "FAIL"
     exit 1
@@ -23,4 +23,4 @@ fi
 
 echo "SUCCESS"
 
-python src/dsopz.py 'export' -d "$DS" -n "$NS" -o true | python src/dsopz.py 'import' -d "$DS" -n "$NS" -o remove
+python dsopz/dsopz.py 'export' -d "$DS" -n "$NS" -o true | python dsopz/dsopz.py 'import' -d "$DS" -n "$NS" -o remove
