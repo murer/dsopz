38affc10169b3284c9a3ebe7b80df1b8ae99d423
murer <murer@dextra-sw.com>
2015-04-13 11:30:54 -0300

    x

---
+++ cmds/pip_upload_prod.sh
@@ -0,0 +1,4 @@
+#!/bin/bash -xe
+
+#python setup.py register -r pypi
+python setup.py upload -r pypi
---
+++ cmds/pip_upload_test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash -xe
+
+#python setup.py register -r pypitest
+python setup.py upload -r pypitest
