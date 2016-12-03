202b21149475cbd96ce7740b6df850d46f75b7e8
murer <murer@dextra-sw.com>
2016-12-03 01:59:43 -0200

    making code better

--- test/test.sh
+++ test/test.sh
@@ -19,6 +19,8 @@ python -m dsopz.dsopz 'export' -d "$DS" -n "$NS" | wc -l | grep '^0$'
 python -m dsopz.dsopz 'import' -d "$DS" -n "$NS" -o upsert < "test/entities.json"
 python -m dsopz.dsopz 'export' -d "$DS" -n "$NS" -o false | diff - "test/entities.json"
 python -m dsopz.dsopz 'export' -d "$DS" -n "$NS" | diff - "test/entities.json"
+python -m dsopz.dsopz 'export' -d "$DS" -n "$NS" -o true | diff - "test/keys.json"
+
 python -m dsopz.dsopz 'gql' -d "$DS" -n "$NS" -q "select * from dsopz_test order by c2" | wc -l | grep '^0$'
 
 python -m dsopz.dsopz 'index' -k "dsopz_test" -c c2 -i true > "$EF/processed.json" < "test/entities.json"
