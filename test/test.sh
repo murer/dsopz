27b142c58759083a40ca03eab4abd22cadd87857
murer <murer@dextra-sw.com>
2016-12-03 01:55:35 -0200

    making code better

--- test/test.sh
+++ test/test.sh
@@ -17,16 +17,15 @@ cleanup
 python -m dsopz.dsopz 'export' -d "$DS" -n "$NS" | wc -l | grep '^0$'
 
 python -m dsopz.dsopz 'import' -d "$DS" -n "$NS" -o upsert < "test/entities.json"
-python -m dsopz.dsopz 'export' -d "$DS" -n "$NS" > "$EF/bak.json"
-diff test/entities.json "$EF/bak.json"
+python -m dsopz.dsopz 'export' -d "$DS" -n "$NS" | diff - "test/entities.json"
 python -m dsopz.dsopz 'gql' -d "$DS" -n "$NS" -q "select * from dsopz_test order by c2" | wc -l | grep '^0$'
 
-python -m dsopz.dsopz 'index' -k "dsopz_test" -c c2 -i true > "$EF/processed.json" < "$EF/bak.json"
+python -m dsopz.dsopz 'index' -k "dsopz_test" -c c2 -i true > "$EF/processed.json" < "test/entities.json"
 python -m dsopz.dsopz 'import' -d "$DS" -n "$NS" -o upsert < "$EF/processed.json"
 python -m dsopz.dsopz 'gql' -d "$DS" -n "$NS" -q "select * from dsopz_test order by c2" | wc -l | grep '^2$'
 python -m dsopz.dsopz 'gql' -d "$DS" -n "$NS" -q "select * from dsopz_test order by c3" | wc -l | grep '^0$'
 
-python -m dsopz.dsopz 'index' -k "dsopz_test" -c c3 -i true > "$EF/processed.json" < "$EF/bak.json"
+python -m dsopz.dsopz 'index' -k "dsopz_test" -c c3 -i true > "$EF/processed.json" < "test/entities.json"
 python -m dsopz.dsopz 'import' -d "$DS" -n "$NS" -o upsert < "$EF/processed.json"
 python -m dsopz.dsopz 'gql' -d "$DS" -n "$NS" -q "select * from dsopz_test where c3 = 'a1'" | wc -l | grep '^2$'
 python -m dsopz.dsopz 'gql' -d "$DS" -n "$NS" -q "select * from dsopz_test where c3 = 'a2'" | wc -l | grep '^1$'
