2ffdcca8350eb9c111b6d0d0e8ec42f48addd550
murer <murer@dextra-sw.com>
2017-06-11 17:10:19 -0300

    release

--- dsopz/exporter.py
+++ dsopz/exporter.py
@@ -26,16 +26,15 @@ def print_iterate(dataset, kinds=[], namespace=None, keys_only=False):
 	if keys_only:
 		field = '__key__'
 	for kind in kinds:
-		reader.print_iterate(dataset, 
-			'select %s from `%s` order by __key__' % (field, kind), 
+		reader.print_iterate(dataset,
+			'select %s from `%s` order by __key__' % (field, kind),
 			namespace=namespace, msg=kind)
 
 def argparse_prepare(sub):
-	sub.add_argument('-d', '--dataset', required=True, help='dataset')
-	sub.add_argument('-n', '--namespace', help='namespace')
-	sub.add_argument('-k', '--kinds', nargs='+', help='kinds')
-	sub.add_argument('-o', '--keys-only', help='keys only')
+	sub.add_argument('-d', '--dataset', required=True, help='dataset (pojectname)')
+	sub.add_argument('-n', '--namespace', help='namespace (default: datastore default namespace)')
+	sub.add_argument('-k', '--kinds', nargs='+', help='kinds (default: all kinds)')
+	sub.add_argument('-o', '--keys-only', help='keys only (default: false)')
 
 def argparse_exec(args):
 	print_iterate(args.dataset, args.kinds, args.namespace, args.keys_only == 'true')
-
