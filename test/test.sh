fc4dc692c6a9a44db6bcc87b9b806c577314e02f
murer <murer@dextra-sw.com>
2017-09-14 11:22:30 -0300

    test

--- test/test.sh
+++ test/test.sh
@@ -17,7 +17,6 @@ kind_test() {
   cleanup
   python -m dsopz.dsopz 'import' -d "$DS" -n "$NS" -o upsert < "test/entities.json"
   python -m dsopz.dsopz 'kind' -d "$DS" -n "$NS" | grep 'dsopz_test'
-  echo aaaaa
 }
 
 import_export_test() {
@@ -41,8 +40,8 @@ python -m dsopz.dsopz 'gql' -d "$DS" -n "$NS" -q "select * from dsopz_test order
 
 index_test() {
 cleanup
-python -m dsopz.dsopz 'index' -k "dsopz_test" -c c2 -i true > "$EF/processed$1.json" < "test/entities.json"
-python -m dsopz.dsopz 'import' -d "$DS" -n "$NS" -o upsert < "$EF/processed$1.json"
+python -m dsopz.dsopz 'index' -k "dsopz_test" -c c2 -i true > "$EF/processed.json" < "test/entities.json"
+python -m dsopz.dsopz 'import' -d "$DS" -n "$NS" -o upsert < "$EF/processed.json"
 sleep 1
 python -m dsopz.dsopz 'gql' -d "$DS" -n "$NS" -q "select * from dsopz_test order by c2" | wc -l | grep '^2$'
 python -m dsopz.dsopz 'gql' -d "$DS" -n "$NS" -q "select * from dsopz_test order by c3" | wc -l | grep '^0$'
@@ -50,8 +49,8 @@ python -m dsopz.dsopz 'gql' -d "$DS" -n "$NS" -q "select * from dsopz_test order
 
 index_list_test() {
 cleanup
-python -m dsopz.dsopz 'index' -k "dsopz_test" -c c3 -i true > "$EF/processed$1.json" < "test/entities.json"
-python -m dsopz.dsopz 'import' -d "$DS" -n "$NS" -o upsert < "$EF/processed$1.json"
+python -m dsopz.dsopz 'index' -k "dsopz_test" -c c3 -i true > "$EF/processed.json" < "test/entities.json"
+python -m dsopz.dsopz 'import' -d "$DS" -n "$NS" -o upsert < "$EF/processed.json"
 sleep 1
 python -m dsopz.dsopz 'gql' -d "$DS" -n "$NS" -q "select * from dsopz_test where c3 = 'a1'" | wc -l | grep '^2$'
 python -m dsopz.dsopz 'gql' -d "$DS" -n "$NS" -q "select * from dsopz_test where c3 = 'a2'" | wc -l | grep '^1$'
@@ -61,7 +60,7 @@ python -m dsopz.dsopz 'gql' -d "$DS" -n "$NS" -q "select * from dsopz_test where
 import_block_test() {
 cleanup
 for k in $(seq 1 7); do cat test/template.json | sed "s/COUNTER/n$k/g"; done | python -m dsopz.dsopz 'import' -d "$DS" -n "$NS" -p 2 -b 2 -o upsert
-python -m dsopz.dsopz 'export' -d "$DS" -n "$NS" | wc -l
+python -m dsopz.dsopz 'export' -d "$DS" -n "$NS" | wc -l | grep '^7$'
 }
 
 offline_test() {
