d841d530b27eb804808f19bb97c343336d41645f
murer <murer@dextra-sw.com>
2018-09-13 15:25:50 -0300

    x

--- dsopz/oauth.py
+++ dsopz/oauth.py
@@ -6,23 +6,33 @@
 #if __name__ == '__main__':
 #	print(auth_file())
 
-
-from dsopz.config import config
-
-_scopes = [
+"""
+[
     'https://www.googleapis.com/auth/cloud-platform',
     'https://www.googleapis.com/auth/datastore',
     'https://www.googleapis.com/auth/userinfo.email'
 ]
+"""
 
-def cmd():
-    print(config.args)
+from dsopz.config import config
 
-subparser = config.add_parser('login', cmd)
-subparser.add_argument('-s', '--scopes', nargs='+', help='scopes', default=_scopes)
+class OAuth(object):
+
+    def login(self):
+        self._scopes = config.args.scopes or [
+            'https://www.googleapis.com/auth/cloud-platform',
+            'https://www.googleapis.com/auth/datastore',
+            'https://www.googleapis.com/auth/userinfo.email'
+        ]
+        print(config.args, self._scopes)
+
+oauth = OAuth()
+
+subparser = config.add_parser('login', oauth.login)
+subparser.add_argument('-s', '--scopes', nargs='+', help='scopes')
 subparser.add_argument('-t', '--text', action='store_true', help='text mode')
 
-subparser = config.add_parser('login-gce', cmd)
+subparser = config.add_parser('login-gce', oauth.login)
 
-subparser = config.add_parser('login-serviceaccount', cmd)
+subparser = config.add_parser('login-serviceaccount', oauth.login)
 subparser.add_argument('-f', '--file', required=True, help='service account json file')
