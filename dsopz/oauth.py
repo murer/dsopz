dd1f72922bd5b1c5ffb7b64f236c570f0ef0a774
murer <murer@dextra-sw.com>
2018-09-13 15:19:19 -0300

    x

--- dsopz/oauth.py
+++ dsopz/oauth.py
@@ -9,12 +9,20 @@
 
 from dsopz.config import config
 
+_scopes = [
+    'https://www.googleapis.com/auth/cloud-platform',
+    'https://www.googleapis.com/auth/datastore',
+    'https://www.googleapis.com/auth/userinfo.email'
+]
+
 def cmd():
     print(config.args)
 
 subparser = config.add_parser('login', cmd)
-subparser.add_argument('-s', '--scopes', nargs='+', help='scopes', default=[
-		'https://www.googleapis.com/auth/cloud-platform',
-		'https://www.googleapis.com/auth/datastore',
-		'https://www.googleapis.com/auth/userinfo.email'
-	])
+subparser.add_argument('-s', '--scopes', nargs='+', help='scopes', default=_scopes)
+subparser.add_argument('-t', '--text', action='store_true', help='text mode')
+
+subparser = config.add_parser('login-gce', cmd)
+
+subparser = config.add_parser('login-serviceaccount', cmd)
+subparser.add_argument('-f', '--file', required=True, help='service account json file')
