4a37c528f2e466c94777f14a000516e1c1922930
murer <murer@dextra-sw.com>
2018-09-13 16:12:07 -0300

    x

--- dsopz/httpreq.py
+++ dsopz/httpreq.py
@@ -1,12 +1,11 @@
 from http.client import HTTPSConnection, HTTPConnection
-import json
+import json as JSON
 from dsopz import util
 import threading
 from urllib.parse import urlparse
 
 class Error(Exception):
 	"""Exceptions"""
-
 
 def req_json(method, url, params = '', headers = {}, expects = [200]):
 	parsed = urlparse(url)
@@ -23,7 +22,6 @@ def req_json(method, url, params = '', headers = {}, expects = [200]):
 		token = parsed.username + ':' + (parsed.password or '')
 		token = 'Basic ' + base64.b64encode(token)
 		headers['Authorization'] = token
-	success = False
 	try:
 		conn.request(method, uri, params, headers)
 		response = conn.getresponse()
@@ -32,50 +30,19 @@ def req_json(method, url, params = '', headers = {}, expects = [200]):
 		string = response.read()
 		if not string:
 			return None
-		ret = json.loads(string.decode('UTF-8'))
-		success = True
-		return ret
+		ret = JSON.loads(string.decode('UTF-8'))
+		return {
+			'status': response.status,
+			'body': ret
+		}
 	finally:
 		util.close(conn)
 
-class HttpThread(threading.Thread):
-
-	def __init__(self, method, url, params, headers, expects):
-		threading.Thread.__init__(self)
-		self.method = method
-		self.url = url
-		self.params = params
-		self.headers = headers
-		self.expects = expects
-		self.response = None
-		self.status = 'CREATED'
-
-	def run(self):
-		try:
-			self.status = 'REQUESTING'
-			self.response = req_json(self.method, self.url, self.params, self.headers, self.expects)
-			self.status = 'DONE'
-		finally:
-			if self.status != 'DONE':
-				self.status = 'ERROR'
-
-	def resp(self):
-		while self.status != 'DONE' and self.status != 'ERROR':
-			self.join()
-		if self.status == 'DONE':
-			return self.response
-		raise Error('Error on http thread, status: %s' % (self.status))
-
-def async_req_json(method, url, params = '', headers = {}, expects = [200]):
-	thread = HttpThread(method, url, params, headers, expects)
-	thread.start()
-	return thread
-
 def __main():
-	obj = async_req_json('GET', 'https://api.github.com/users/murer/keys', headers = {
+	obj = req_json('GET', 'https://api.github.com/users/murer/keys', headers = {
 		'User-Agent': 'dsopz'
-	}, expects = [200]).resp()
-	print(json.dumps(obj, indent=True))
+	}, expects = [200])
+	print(obj)
 
 if __name__ == '__main__':
 	__main()
