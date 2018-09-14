c55ba76cde77eb6cca2c395d012c0f6dfe7285b4
murer <murer@dextra-sw.com>
2018-09-14 10:31:15 -0300

    x

--- dsopztest/__main__.py
+++ dsopztest/__main__.py
@@ -3,7 +3,7 @@ from unittest import TestLoader, TextTestRunner
 
 def main():
     loader = TestLoader()
-    suite = loader.discover('sampletest', pattern='*_test.py')
+    suite = loader.discover('dsopztest', pattern='*_test.py')
     runner = TextTestRunner(verbosity=2, failfast=True)
     success = runner.run(suite).wasSuccessful()
     if not success:
