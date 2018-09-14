740a674ffe1725508acd86d003693cd42ae7af11
murer <murer@dextra-sw.com>
2018-09-14 11:48:49 -0300

    x

--- dsopztest/processor_test.py
+++ dsopztest/processor_test.py
@@ -1,20 +1,28 @@
 import unittest
+import time
 from dsopz.processor import Processor
 from concurrent.futures import ThreadPoolExecutor
 
-def task(a, b):
-    ret = a + b
-    print('task', ret)
-    return ret
+class Task(object):
+
+    def __init__(self):
+        self.total = 0
+
+    def task(self, a):
+        time.sleep(1)
+        print('sum', a)
+        self.total = self.total + a
 
 class TestCase(unittest.TestCase):
 
     def test_job(self):
+        task = Task()
         with Processor(1) as p:
-            p.submit(task, 3, 4)
-            p.submit(task, 3, 4)
-            p.submit(task, 3, 4)
-            p.submit(task, 3, 4)
+            p.submit(task.task, 1)
+            p.submit(task.task, 2)
+            p.submit(task.task, 4)
+            p.submit(task.task, 8)
+        self.assertEqual(15, task.total)
 
 if __name__ == '__main__':
     unittest.main()
