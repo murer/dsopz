cb144bc245105775cbfa0208b1505edc44375320
murer <murer@dextra-sw.com>
2018-09-18 10:44:33 -0300

    x

--- dsopz/datastore.py
+++ dsopz/datastore.py
@@ -1,31 +1,30 @@
 from dsopz.http import req_json
 from dsopz.oauth import oauth
 from dsopz.config import config
+import json as JSON
 
 def run_query(
         dataset,
         namespace,
-        gql,
-        offset = None,
-        limit = None,
-        start_cusror = None,
-        end_cursor = None):
-
+        query):
     url = '%s/v1/projects/%s:runQuery' % (config.args.url, dataset)
-    gql_full = gql
     body = {
         'partitionId': {
             'projectId': dataset,
             'namespaceId': namespace
-        },
-        'gqlQuery': {
-            'allowLiterals': True,
-            'queryString': gql_full
         }
     }
+    if isinstance(query, dict):
+        body['query'] = query
+    else:
+        body['gqlQuery'] = {
+            'allowLiterals': True,
+            'queryString': query
+        }
     resp = req_json('POST', url, body, {
         'Authorization': 'Bearer %s' % (oauth.access_token())
     })
     ret = resp['body']
     ret['batch']['entityResults'] = ret['batch'].get('entityResults', [])
+    ret['query'] = ret.get('query', query)
     return ret
--- dsopztest/datastore_test.py
+++ dsopztest/datastore_test.py
@@ -1,19 +1,22 @@
 import unittest
 from dsopztest import abstract_test_case
 from dsopz import datastore as ds
+import json as JSON
 
 class TestCase(abstract_test_case.TestCase):
 
     def test_run_query(self):
-        result = ds.run_query(
-            'dsopzproj',
-            '',
-            'select * from notfound where x = 2',
-            0,
-            10,
-            'startcursor',
-            'endcursor'
-        )
+        result = ds.run_query('dsopzproj', '', 'select * from notfound where x = 2 and y = 1')
+        self.assertEqual(0, len(result['batch']['entityResults']))
+        self.assertEqual('NO_MORE_RESULTS', result['batch']['moreResults'])
+        self.assertIsNotNone(result['batch']['endCursor'])
+
+        result = ds.run_query('dsopzproj', '', result['query'])
+        self.assertEqual(0, len(result['batch']['entityResults']))
+        self.assertEqual('NO_MORE_RESULTS', result['batch']['moreResults'])
+        self.assertIsNotNone(result['batch']['endCursor'])
+        
+        result = ds.run_query('dsopzproj', '', result['query'])
         self.assertEqual(0, len(result['batch']['entityResults']))
         self.assertEqual('NO_MORE_RESULTS', result['batch']['moreResults'])
         self.assertIsNotNone(result['batch']['endCursor'])
