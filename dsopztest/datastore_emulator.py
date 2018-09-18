7cc8da14b35c809d27d10c0e9f489d605d018dce
murer <murer@dextra-sw.com>
2018-09-18 16:58:57 -0300

    x

--- dsopztest/datastore_emulator.py
+++ dsopztest/datastore_emulator.py
@@ -1,12 +1,16 @@
 import subprocess
-
-
+import time
+from dsopz.http import req_json
 
 def main():
-    x = subprocess.Popen(
-        [ 'gcloud', 'beta', 'emulators', 'datastore', 'start', '--host-port', 'localhost:8082', '--no-store-on-disk' ],
-        stdin=subprocess.PIPE, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
-    print('xxx', x)
+    server = subprocess.Popen(
+        [ 'gcloud', 'beta', 'emulators', 'datastore', 'start', '--host-port', 'localhost:8082', '--no-store-on-disk' ]
+    )
+    print('xxx', server)
+    time.sleep(5)
+    req_json('POST', 'http://localhost:8082/shutdown', '')
+    time.sleep(5)
+    server.kill()
 
 if __name__ == '__main__':
     main()
