bea2ded1282ddcfecd3b49b20137d29099b15c86
murer <murer@dextra-sw.com>
2018-09-20 14:16:39 -0300

    x

--- dsopztest/abstract_test_case.py
+++ dsopztest/abstract_test_case.py
@@ -2,18 +2,22 @@ import unittest
 import shutil
 import os
 from dsopz.config import config
+import json as JSON
 from dsopztest.datastore_emulator import DSEmulator
+from dsopz.oauth import oauth
 
 class Error(Exception):
     """Exceptions"""
 
 class TestCase(unittest.TestCase):
 
-    def configure(self):
-        """ noop """
-
     def setUp(self):
-        config.parse_args([ '-u', 'http://localhost:8082', 'noop' ])
+        config.parse_args([ '-u', 'http://localhost:8082', '-a', './gen/auth/oauth.json', 'noop' ])
+        oauth._config()
+        oauth._write_file({
+            "expires" : 1537467118,
+            "access_token" : "dummytoken"
+        })
         self.server = DSEmulator()
         self.server.start()
 
