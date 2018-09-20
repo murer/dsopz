71a47c68ca1b01902f56ef8ced8dc25ecd0723db
murer <murer@dextra-sw.com>
2018-09-20 13:34:28 -0300

    x

--- ci/before_script.sh
+++ ci/before_script.sh
@@ -3,3 +3,15 @@
 echo "branch: $TRAVIS_BRANCH"
 
 ./cmds/decrypt.sh
+
+if ! ./gen/google-cloud-sdk/bin/gcloud --version; then
+  rm -rf gen/google-cloud-sdk || true
+  rm google-cloud-sdk.tar.gz || true
+  mkdir gen || true
+  wget 'https://repoz.dextra.com.br/repoz/r/pub/google-cloud/google-cloud-sdk-114.0.0-linux-x86_64.tar.gz' -O gen/google-cloud-sdk.tar.gz
+  cd gen
+  tar xzf google-cloud-sdk.tar.gz
+  cd -
+fi
+
+./gen/google-cloud-sdk/bin/gcloud components update -q
