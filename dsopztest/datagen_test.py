032b29578a1151dcf44ce42e316175aadfc61175
murer <murer@dextra-sw.com>
2018-09-21 10:56:43 -0300

    x

--- dsopztest/datagen_test.py
+++ dsopztest/datagen_test.py
@@ -1,10 +1,17 @@
 import unittest
 
+class Error(Exception):
+	"""Exceptions"""
+
 def key(k, dataset='dsopzproj', namespace=None):
+    if len(k) % 2 != 0:
+        raise Error('must be even %s' % (k))
     ret = {
         'partitionId': { 'projectId': dataset },
-        'path': [ { 'kind': k[0], 'name': k[1] }]
+        'path': []
     }
+    for i in range(0, len(k), 2):
+        ret['path'].append({ 'kind': k[i], 'name': k[i+1] })
     if namespace:
         ret['partitionId']['namespaceId'] = namespace
     return ret
@@ -20,6 +27,10 @@ class DatagenTest(unittest.TestCase):
             'partitionId': { 'projectId': 'x', 'namespaceId': 'y' },
             'path': [ { 'kind': 'hero', 'name': 'ana' }]
         }, key(('hero', 'ana'), dataset='x', namespace='y'))
+        self.assertEqual({
+            'partitionId': { 'projectId': 'dsopzproj' },
+            'path': [ {'kind': 'x', 'name':  'y'}, { 'kind': 'hero', 'name': 'ana' }]
+        }, key(('x', 'y', 'hero', 'ana')))
 
 if __name__ == '__main__':
     unittest.main()
