321fe43f8a24db298411e869027ac185d4230803
murer <murer@dextra-sw.com>
2018-09-25 16:48:32 -0300

    test

--- dsopz/cmd/query.py
+++ dsopz/cmd/query.py
@@ -6,7 +6,7 @@ import json as JSON
 def cmd_query():
     result = stream_entity(config.args.dataset, config.args.namespace, config.args.gql)
     query = next(result)
-    print(JSON.dumps({'query': query, 'dataset': dataset, 'namespace': namespace}))
+    print(JSON.dumps({'query': query, 'dataset': config.args.dataset, 'namespace': config.args.namespace}))
     for entity in result:
         print(JSON.dumps(entity))
 
@@ -24,6 +24,15 @@ def cmd_resume():
     for entity in result:
         print(JSON.dumps(entity))
 
+def cmd_kind():
+    result = stream_entity(config.args.dataset, config.args.namespace, 'select __key__ from __kind__')
+    query = next(result)
+    for entity in result:
+        k = entity['entity']['key']
+        name = k['path'][0]['name']
+        if name.startswith('__') and not config.args.all:
+            continue
+        print(name)
 
 
 subparser = config.add_parser('query', cmd_query)
@@ -32,3 +41,8 @@ subparser.add_argument('-n', '--namespace', help='namespace')
 subparser.add_argument('-g', '--gql', required=True, help='gql')
 
 subparser = config.add_parser('resume', cmd_resume)
+
+subparser = config.add_parser('kind', cmd_kind)
+subparser.add_argument('-d', '--dataset', required=True, help='dataset')
+subparser.add_argument('-n', '--namespace', help='namespace')
+subparser.add_argument('-a', '--all', action='store_true', help='print "__.*__" also')
