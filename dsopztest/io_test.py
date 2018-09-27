aec5d8f74077a06a4e0b8e602ce9119eb5fa7592
murer <murer@dextra-sw.com>
2018-09-27 13:35:20 -0300

    test

--- dsopztest/io_test.py
+++ dsopztest/io_test.py
@@ -5,13 +5,6 @@ from dsopz import util
 
 class ReaderWriterTest(unittest.TestCase):
 
-    def setUp(self):
-        util.makedirs('target/sandbox')
-        with open('target/sandbox/data.txt', 'wb') as f:
-            f.write(b'line1\nline2\n')
-        with gzip.open('target/sandbox/data.txt.gz', 'wb') as f:
-            f.write(b'line1\nline2\n')
-
     def test_stream_read_plain(self):
         with io.Writer('target/sandbox/data.txt', False) as f:
             f.write('line1')
@@ -21,6 +14,9 @@ class ReaderWriterTest(unittest.TestCase):
             self.assertEqual(['line1', 'line2'], lines)
 
     def test_stream_read_gz(self):
+        with io.Writer('target/sandbox/data.txt.gz', True) as f:
+            f.write('line1')
+            f.write('line2')
         with io.Reader('target/sandbox/data.txt.gz', True) as f:
             lines = [l for l in f]
             self.assertEqual(['line1', 'line2'], lines)
