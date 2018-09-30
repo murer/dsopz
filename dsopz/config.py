860e1922390e61c96170e75469a22129fcf91fee
murer <murer@dextra-sw.com>
2018-09-29 21:23:36 -0300

    test

--- dsopz/config.py
+++ dsopz/config.py
@@ -25,28 +25,3 @@ class Config(object):
 		mod()
 
 config = Config()
-
-"""
-	sub.add_argument('-d', '--dataset', required=True, help='dataset')
-	sub.add_argument('-n', '--namespace', help='namespace')
-	sub.add_argument('-p', '--no-prompt', dest='prompt', action='store_false', help='no prompt')
-	sub.add_argument('-sl', '--single-line', dest='single_line', action='store_true', help='single line')
-	sub.add_argument('-ns', '--no-summary', dest='no_summary', action='store_true', help='single line')
-	sub.add_argument('-l', '--limit', help='limit', type=int)
-	sub.add_argument('-s', '--seperator', help='seperator')
-"""
-
-def main():
-	parser = argparse.ArgumentParser(description='DSOpz')
-	subparsers = parser.add_subparsers(dest="subparsers", title="Command")
-	sub = subparsers.add_parser('login')
-	sub.add_argument('-s', '--scopes', nargs='+', help='scopes', default=[
-		'https://www.googleapis.com/auth/cloud-platform',
-		'https://www.googleapis.com/auth/datastore',
-		'https://www.googleapis.com/auth/userinfo.email'
-	])
-	args = parser.parse_args()
-	log.info(args)
-
-if __name__ == '__main__':
-	log.info(main())
