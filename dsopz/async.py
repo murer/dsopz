603ccdf7e5b4cee1e72d1e9d75acff5ecb49ba0a
murer <murer@dextra-sw.com>
2018-10-01 14:44:06 -0300

    test

--- dsopz/async.py
+++ dsopz/async.py
@@ -3,9 +3,13 @@ from concurrent.futures import Future
 
 def _work(future, fn, args, kwargs):
     future.set_running_or_notify_cancel()
-    ret = fn(*args, **kwargs)
-    future.set_result(ret)
-
+    try:
+        ret = fn(*args, **kwargs)
+    except BaseException as exc:
+        future.set_exception(exc)
+    else:
+        future.set_result(result)
+    
 def async(fn, *args, **kwargs):
     ret = Future()
     thread = threading.Thread(target=_work, args=[ret, fn, args, kwargs])
