25f556bddee83e425fb7455d58c363d96af8340c
murer <murer@dextra-sw.com>
2018-10-01 14:54:05 -0300

    test

--- dsopztest/http_test.py
+++ dsopztest/http_test.py
@@ -8,9 +8,14 @@ class HttpTest(abstract_test_case.TestCase):
 
     def test_sync(self):
         self.assertEqual(404, http.req_text('POST', 'http://localhost:8082/not-found', expects=[404])['status'])
+
         self.assertEqual(404, async(
             http.req_text, 'POST', 'http://localhost:8082/not-found', expects=[404]
         ).result()['status'])
+
+        self.assertIsInstance(async(
+            http.req_text, 'POST', 'http://localhost:8082/not-found'
+        ).exception(), http.Error)
 
 
 
