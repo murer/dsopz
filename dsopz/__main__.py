28d762753cef60bb68e016c4136889e9371160c6
murer <murer@dextra-sw.com>
2018-10-02 15:33:09 -0300

    test

--- dsopz/__main__.py
+++ dsopz/__main__.py
@@ -1,11 +1,4 @@
-
-from dsopz import oauth
-from dsopz.cmd import basic
-
-from dsopz.config import config
-
-def main():
-    config.parse_args()
+from dsopz.main import main
 
 if __name__ == '__main__':
     main()
---
+++ dsopz/main.py
@@ -0,0 +1,11 @@
+
+from dsopz import oauth
+from dsopz.cmd import basic
+
+from dsopz.config import config
+
+def main():
+    config.parse_args()
+
+if __name__ == '__main__':
+    main()
