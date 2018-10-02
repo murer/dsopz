a3f1573e886155100c7a103fe7154f862039cacc
murer <murer@dextra-sw.com>
2018-10-02 14:50:53 -0300

    test

--- cmds/release.sh
+++ cmds/release.sh
@@ -13,7 +13,7 @@ fi
 
 #python -m dsopz.dsopz version
 
-echo "version=\"$CLOSE_VERSION\"" > dsopz/config.py
+#echo "version=\"$CLOSE_VERSION\"" > dsopz/config.py
 git commit -am "releasing $CLOSE_VERSION"
 git tag "dsopz-$CLOSE_VERSION"
 git push origin "dsopz-$CLOSE_VERSION"
