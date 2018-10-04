b35968aa3fe922f92941ac0c0d38ddedc1e7a5c9
murer <murer@dextra-sw.com>
2018-10-04 16:08:57 -0300

    ci

--- dsopz/processor.py
+++ dsopz/processor.py
@@ -19,6 +19,21 @@ def dispatch(fn, *args, **kwargs):
     thread.start()
     return ret
 
+def _work_with_future(future, fn, args, kwargs):
+    future.set_running_or_notify_cancel()
+    try:
+        ret = fn(future, *args, **kwargs)
+    except BaseException as exc:
+        future.set_exception(exc)
+    else:
+        future.set_result(ret)
+
+def dispatch_with_future(fn, *args, **kwargs):
+    ret = Future()
+    thread = threading.Thread(target=_work_with_future, args=[ret, fn, args, kwargs])
+    thread.start()
+    return ret
+
 def blockify(array, block_size, filter=None, skip=0):
     ret = []
     count = 0
@@ -49,7 +64,25 @@ def merge_gens(arrays):
         if stop:
             return
 
+def _async_gen_work(gen, q):
+    for i in gen:
+        print('put', i)
+        q.put((False, i))
+    q.put((True, None))
+
 def async_gen(gen, maxsize=10):
+    from queue import Queue
+    q = Queue(maxsize=maxsize)
+    fut = dispatch_with_future(_async_gen_work, gen, q)
+    while True:
+        done, ret = q.get()
+        print('get', done, ret, fut.done())
+        fut.cancel()
+        if done:
+            return
+        yield ret
+
+    """
     elements = []
     try:
         while True:
@@ -62,3 +95,4 @@ def async_gen(gen, maxsize=10):
     finally:
         while len(elements) > 0:
             elements.pop(0).exception()
+    """
