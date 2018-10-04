1f700e53dbff84ad5f35ed9f159bcfbe6bb24211
murer <murer@dextra-sw.com>
2018-10-04 16:25:53 -0300

    test

--- dsopz/processor.py
+++ dsopz/processor.py
@@ -1,5 +1,6 @@
 import threading
 from concurrent.futures import Future
+from queue import Queue
 
 class Error(Exception):
     """Exceptions"""
@@ -16,21 +17,6 @@ def _work(future, fn, args, kwargs):
 def dispatch(fn, *args, **kwargs):
     ret = Future()
     thread = threading.Thread(target=_work, args=[ret, fn, args, kwargs])
-    thread.start()
-    return ret
-
-def _work_with_future(future, fn, args, kwargs):
-    future.set_running_or_notify_cancel()
-    try:
-        ret = fn(future, *args, **kwargs)
-    except BaseException as exc:
-        future.set_exception(exc)
-    else:
-        future.set_result(ret)
-
-def dispatch_with_future(fn, *args, **kwargs):
-    ret = Future()
-    thread = threading.Thread(target=_work_with_future, args=[ret, fn, args, kwargs])
     thread.start()
     return ret
 
@@ -71,28 +57,12 @@ def _async_gen_work(gen, q):
     q.put((True, None))
 
 def async_gen(gen, maxsize=10):
-    from queue import Queue
     q = Queue(maxsize=maxsize)
-    fut = dispatch_with_future(_async_gen_work, gen, q)
+    fut = dispatch(_async_gen_work, gen, q)
     while True:
         done, ret = q.get()
         print('get', done, ret, fut.done())
-        fut.cancel()
         if done:
+            fut.result()
             return
         yield ret
-
-    """
-    elements = []
-    try:
-        while True:
-            while len(elements) < maxsize:
-                elements.append(dispatch(lambda x: next(x), gen))
-            element = elements.pop(0)
-            yield element.result()
-    except StopIteration:
-        pass
-    finally:
-        while len(elements) > 0:
-            elements.pop(0).exception()
-    """
