92025fdbae5fe5194d59443060e6967f2fd02ac9
murer <murer@dextra-sw.com>
2021-12-06 14:21:16 -0300

    x

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM ubuntu:16.04
+
+RUN apt-get -y update && \
+    apt-get -y install python
---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -xe
+
+cmd_clean() {
+    docker ps -aq --filter label=dsopz_dev | xargs docker rm -f || true
+    docker system prune --volumes --filter label=dsopz_dev -f || true
+}
+
+cmd_docker_build() {
+    docker build -t dsopz/dsopz:dev .
+}
+
+cmd_docker_run() {
+    cmd_docker_build
+    docker volume create --label dsopz_dev dsopz_config || true
+    docker run -it --rm --label dsopz_dev \
+        -v "$(pwd):/opt/dsopz" \
+        -w "/opt/dsopz" \
+        --mount "type=volume,src=dsopz_config,dst=/root/.dsopz" \
+        dsopz/dsopz:dev "$@"
+        #python:3.8-buster "$@"
+}
+
+cmd_docker_login() {
+    cmd_docker_run python -m dsopz.dsopz login-text
+}
+
+cd "$(dirname "$0")"; _cmd="${1?"cmd is required"}"; shift; "cmd_${_cmd}" "$@"
